6cafa3a29959087872a6d903e16e373629fe2c32
ThienDuc1112 <75243419+truongnvgcd201597@users.noreply.github.com>
2022-06-27 11:01:37 +0700

    Create Register Controller and View

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -22,4 +22,11 @@ class FrontController extends AbstractController
     {
         return $this->render('front/login.html.twig');
     }
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
 }
