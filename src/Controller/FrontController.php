b667efc3fc91a076d09284932968ad76967b9ccd
Linhvjc <93339285+Linhvjc@users.noreply.github.com>
2022-07-07 00:32:42 +0700

    Add role admin and user and custom view
    User can't view Admin panel
    The view will be different between visitor and the person who logged in.
    Display error message when user doesn't exist
    
    Co-Authored-By: ThienDuc1112 <95234772+ThienDuc1112@users.noreply.github.com>
    Co-Authored-By: @nv_truonqqq <75243419+truongnvgcd201597@users.noreply.github.com>

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -6,6 +6,7 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FrontController extends AbstractController
 {
@@ -20,9 +21,19 @@ class FrontController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function login()
+    public function login(AuthenticationUtils $helper)
     {
-        return $this->render('front/login.html.twig');
+        return $this->render('front/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout() : void
+    {
+        throw new \Exception('This should never be reached!');
     }
 
     /**
